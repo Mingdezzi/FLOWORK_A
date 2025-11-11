3d5365200acb422c9be22f82e085875c1c93c675
Mingdezzi <107346824+Mingdezzi@users.noreply.github.com>
2025-11-12 03:35:06 +0900

    Add files via upload

--- flowork/blueprints/ui/__init__.py
+++ flowork/blueprints/ui/__init__.py
@@ -2,8 +2,5 @@ from flask import Blueprint
 
 ui_bp = Blueprint('ui', __name__, template_folder='../../templates')
 
-# 뷰 모듈
+# Import the routes that belong to the UI blueprint
 from . import main, product, order, sales, admin
-
-# 에러 핸들러 및 컨텍스트 프로세서 등록 (Import만 하면 데코레이터에 의해 등록됨)
-from . import errors, processors
