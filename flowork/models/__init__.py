68ab05edf216d9c17e8b05ffda3a1785a2e02c8f
Mingdezzi <107346824+Mingdezzi@users.noreply.github.com>
2025-11-13 15:20:55 +0900

    Add files via upload

--- flowork/models/__init__.py
+++ flowork/models/__init__.py
@@ -1,7 +1,5 @@
 from ..extensions import db
 from .auth import Brand, User
 from .store import Store, Staff, ScheduleEvent, Setting, Announcement
-from .product import Product, Variant, StoreStock
+from .product import Product, Variant, StoreStock, StockHistory
 from .sales import Order, OrderProcessing, Sale, SaleItem
-
-# 기존 models.py 역할을 이 파일이 대신합니다.
