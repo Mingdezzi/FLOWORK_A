b4d22d006505a5f2921c33ed7e4c76010c482d97
Mingdezzi <mingventador@gmail.com>
2025-11-25 14:45:24 +0900

    RESET

--- flowork/blueprints/ui/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-ui_bp = Blueprint('ui', __name__, template_folder='../../templates')
-
-from . import main, product, order, sales, admin, errors, processors, stock_transfer, crm, operations, network, store_order, online
--- flowork/services/brand_logic/__init__.py
+++
@@ -1,9 +0,0 @@
-from . import generic, eider
-
-LOGIC_MAP = {
-    'GENERIC': generic,
-    'EIDER': eider,
-}
-
-def get_brand_logic(logic_name):
-    return LOGIC_MAP.get(logic_name, generic)
